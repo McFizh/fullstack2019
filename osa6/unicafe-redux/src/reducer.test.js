62ac3e5484cc21d0fa0ff8ddd92feb279d082f8b
Pekka Harjamäki <spamtrap_github@mcfish.org>
2019-06-29 22:23:15 +0300

    Osa 6 : Tehtävät 6.1 ja 6.2

--- osa6/unicafe-redux/src/reducer.test.js
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,48 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('zero works', () => {
+    const state = initialState
+
+    deepFreeze(state)
+    const newState1 = counterReducer(state, {action: 'BAD'})
+    const newState2 = counterReducer(newState1, {action: 'ZERO'})
+    expect(newState2).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+
 })
