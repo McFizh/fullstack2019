b3e9be3530d677d80eb0859fcd60ea8851a93b76
Pekka Harjamäki <spamtrap_github@mcfish.org>
2019-09-15 22:48:46 +0300

    Osa 7 : Tehtävä 7.16

--- osa7/blista-front-webpack/webpack.config.js
+++ osa7/blista-front-webpack/webpack.config.js
@@ -1,15 +1,19 @@
-const path = require('path')
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const config = {
-  entry: './src/index.js',
+  entry: ['babel-polyfill', './src/index.js'],
+
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'main.js'
   },
+
   module: {
     rules: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
           presets: ['@babel/preset-env', '@babel/preset-react']
@@ -22,18 +26,35 @@ const config = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                includePaths: ['./node_modules','./src']
+              }
+            }
+          }
         ],
       },
     ],
   },
+
+  plugins: [
+    new CopyPlugin([
+      { from: 'static' }
+    ])
+  ],
+
   devServer: {
     contentBase: path.resolve(__dirname, 'build'),
     compress: true,
+    proxy: {
+      '/api': 'http://localhost:3003'
+    },
     port: 3000,
   },
+
   devtool: 'source-map',
 }
 
