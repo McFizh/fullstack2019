766888585030439055de385ee0f24fab14205eb8
Pekka Harjamäki <spamtrap_github@mcfish.org>
2019-10-06 22:18:30 +0300

    Osa 8 : Tehtävä 8.17 - 8.18

--- osa8/frontend/src/components/header.js
+++ osa8/frontend/src/components/header.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 import { Link } from 'react-router-dom'
 
-const Header = () => {
+const Header = ({ authToken, setAuthToken }) => {
+  const logout = (e) => {
+    e.preventDefault();
+    setAuthToken('');
+  }
+
+  const newBookLink = authToken === '' ? null : <Link to="/addbook">New book</Link>;
+  const sessionLink = authToken !== '' ?
+    <button onClick={logout}>Logout</button> :
+    <Link to="/login">Login</Link>;
+
   return <div>
     <Link to="/">Books</Link> &nbsp;
     <Link to="/authors">Authors</Link> &nbsp;
-    <Link to="/addbook">New book</Link> &nbsp;
+    {newBookLink}
+    &nbsp; | &nbsp;
+    {sessionLink}
     <hr/>
   </div>;
 }
